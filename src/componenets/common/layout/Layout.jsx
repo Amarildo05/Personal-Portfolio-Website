ad64d3e8268ca90dde82e1406e2fab58dff6f130
Amarildo <amarildokola36@gmail.com>
2025-07-30 23:47:24 +0200

    little fix about the routing

--- src/componenets/common/layout/Layout.jsx
+++ src/componenets/common/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../ScrollToTop";
 
 export default function Layout() {
   return (
     <div>
       <Navbar />
       <main>
+        <ScrollToTop />
         <Outlet /> {/* Content for the current route */}
       </main>
       <Footer />
